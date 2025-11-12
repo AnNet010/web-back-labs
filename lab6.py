27d27bcdcf0ca65d0252cb1b4dd734ff7e3080a8
Анна Боброва <annabobrova05@yandex.ru>
2025-11-12 17:24:39 +0700

    Исправления

--- lab6.py
+++ lab6.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-lab3 = Blueprint('lab6', __name__)
+lab6 = Blueprint('lab6', __name__)
 
-@lab3.route('/lab6/')
+@lab6.route('/lab6/')
 def main():
     return render_template('lab6/lab6.html')
