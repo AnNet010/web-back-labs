9efd3d7d69efb25a1135f02177290566eb88987e
Анна Боброва <annabobrova05@yandex.ru>
2025-11-27 09:21:32 +0700

    Редактирование выбранного фильма по REST

--- lab7.py
+++ lab7.py
@@ -87,9 +87,18 @@ def get_film(id):
     return films[id]
 
 @lab7.route('/lab7/rest-api/films/<int:id>', methods=['DELETE'])
-def def_film(id):
+def del_film(id):
     if id < 0 or id >= len(films):
         abort(404)
 
     del films[id]
     return '', 204
+
+@lab7.route('/lab7/rest-api/films/<int:id>', methods=['PUT'])
+def put_film(id):
+    if id < 0 or id >= len(films):
+        abort(404)
+
+    film = request.get_json()
+    films[id] = film
+    return films[id]
