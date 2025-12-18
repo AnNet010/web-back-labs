78efafc40939e90bab307adf4e625ee1e36d60b3
Анна Боброва <annabobrova05@yandex.ru>
2025-12-18 15:00:17 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, session, current_app, abo
 from werkzeug.security import check_password_hash, generate_password_hash
 from db import db
 from db.models import users, articles
+from flask_login import login_user, login_required, current_user
 from datetime import datetime
 import psycopg2
 from psycopg2.extras import RealDictCursor
@@ -51,7 +52,7 @@ def register():
 
     login_exists = users.query.filter_by(login = login_form).first()
     if login_exists:
-        return render_template('lab8/register.html,',
+        return render_template('lab8/register.html',
                                error = 'Такой пользователь уже существует')
        
     password_hash = generate_password_hash(password_form)
@@ -59,4 +60,33 @@ def register():
     db.session.add(new_user)
     db.session.commit()
     return redirect('/lab8/')
+
+
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    if not login_form or not password_form:
+        return render_template('/lab8/login.html',
+                               error='Логин и пароль не могут быть пустыми')
+    
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember = False)
+            session['login'] = login_form
+            return redirect('/lab8/')
+
+    return render_template('/lab8/login.html',
+                           error = 'Ошибка входа: логин и/или пароль неверны') 
+
+@lab8.route('/lab8/articles')
+@login_required
+def article_list():
+    return "список статей"
 
